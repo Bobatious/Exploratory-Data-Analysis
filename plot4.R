849783c1cdd198cff5955abc150a61573476e27d
Robert Deng <deng.bobby@gmail.com>
2016-07-17 19:31:42 -0700

    Course Project 1

--- plot4.R
+++ plot4.R
@@ -34,7 +34,7 @@ plot(powers$postime, powers$Global_active_power, type = "l", xlab = "", ylab = "
 plot(powers$postime, powers$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering", col = "black")
 lines(powers$postime, powers$Sub_metering_2, col = "red")
 lines(powers$postime, powers$Sub_metering_3, col = "blue")
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = 1)
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="n")
 
 
 #Top Right Plot
